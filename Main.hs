6184d7eb452531b9d7f1b4e7fe3b3da9ccd4cdcb
Jake Rosen <jakesrosen@gmail.com>
2018-07-25 10:53:45 -0700

    initial commit

---
+++ Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Reactive.Banana
+import Reactive.Banana.Frameworks
+import qualified Termbox.Banana as TB
+
+-- main :: (width ~ Int, height ~ Int)
+-- => InputMode
+-- -> OutputMode
+-- -> (Event TermboxEvent -> Behavior (width, height) -> MomentIO (Behavior Scene, Event a))
+-- -> IO a
+
+main :: IO ()
+main = putStrLn "hi"
